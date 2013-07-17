a731ed632f1fce4b9f71895fb808245158b09044
Batman123579 <th3bathead@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2013-07-17 20:19:21 +0100

    1st code!
    Setting up mod.

---
+++ src/minecraft/batman123579/moarores/client/ClientProxy.java
@@ -0,0 +1,11 @@
+package batman123579.moarores.client;
+
+import batman123579.moarores.common.CommonProxy;
+import net.minecraftforge.client.MinecraftForgeClient;
+
+public class ClientProxy extends CommonProxy {
+
+public void registerRenderInformation(){
+}
+
+}
